eab97ffd0370e52edd6c66671c0f09357f1107d7
devsayemmolla <98878071+devsayemmolla@users.noreply.github.com>
2022-11-08 10:49:48 +0600

    basic routes setup done

---
+++ src/Routes/Route/Route.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Main from '../../layouts/Main/Main';
+
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>
+    }
+])
+
+
+const Route = () => {
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Route;
