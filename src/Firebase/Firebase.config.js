7b449a78082ac55f68804710847de9202a5bf76e
devsayemmolla <98878071+devsayemmolla@users.noreply.github.com>
2022-12-21 23:44:58 +0600

    all bugs fixed

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,14 +5,20 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+    appId: process.env.REACT_APP_APPP_ID,
+    apiKey: "AIzaSyAhMPMRlExzuDZBSXbwXN9q8duH9fdkmoU",
+    authDomain: "graphic-assainment-11.firebaseapp.com",
+    projectId: "graphic-assainment-11",
+    storageBucket: "graphic-assainment-11.appspot.com",
+    messagingSenderId: "741879255636",
+    appId: "1:741879255636:web:3af241e8dc192545eb13c1"
 };
-
+console.log(process.env.REACT_APP_API_KEY)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
--- src/pages/Home/ContactUs/ContactUs.js
+++ src/pages/Home/ContactUs/ContactUs.js
@@ -15,7 +15,7 @@ const ContactUs = () => {
                 <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
                     <Link to="" className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
                         <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                         </svg>
 
                         <span className="mt-2">Dhaka , Bangladesh</span>
@@ -39,33 +39,33 @@ const ContactUs = () => {
                     </Link>
                 </div>
 
-                <div className="mt-6 ">
+                <form className="mt-6 " action="https://formsubmit.co/graphical@email.com" method="POST" >
                     <div className="items-center -mx-2 md:flex">
                         <div className="w-full mx-2">
                             <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">Name</label>
 
-                            <input className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" type="text" />
+                            <input name='name' className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" type="text" />
                         </div>
 
                         <div className="w-full mx-2 mt-4 md:mt-0">
                             <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">E-mail</label>
 
-                            <input className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" type="email" />
+                            <input name='email' className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" type="email" />
                         </div>
                     </div>
 
                     <div className="w-full mt-4">
                         <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">Message</label>
 
-                        <textarea className="block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
+                        <textarea name='massage' className="block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
                     </div>
 
                     <div className="flex justify-center mt-6">
-                        <Button className="px-4 py-2 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Send
+                        <Button type='submit' className="px-4 py-2 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Send
                             Message
                         </Button>
                     </div>
-                </div>
+                </form>
             </section>
         </div>
     );
